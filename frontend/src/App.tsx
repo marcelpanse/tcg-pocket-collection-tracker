3fc90bc4988fc5eb8fcbe2ba829655aa7183637b
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2025-02-05 15:00:33 +0100

    ✨ feat: integrate owned cards management and enhance user experience in collection overview

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,7 @@
 import { ThemeProvider } from '@/components/theme-provider'
+import { COLLECTION_ID, DATABASE_ID, getDatabase } from '@/lib/Auth'
 import { getUser } from '@/lib/Auth.ts'
+import type { CollectionRow } from '@/types'
 import type { Models } from 'appwrite'
 import { useEffect, useState } from 'react'
 import { Route, Routes } from 'react-router-dom'
@@ -13,6 +15,7 @@ import { Verify } from './pages/Verify.tsx'
 
 function App() {
   const [user, setUser] = useState<Models.User<Models.Preferences> | null>(null)
+  const [ownedCards, setOwnedCards] = useState<CollectionRow[]>([])
 
   useEffect(() => {
     getUser().then((user) => {
@@ -22,14 +25,27 @@ function App() {
     })
   }, [])
 
+  useEffect(() => {
+    if (user) {
+      fetchCollection()
+    }
+  }, [user])
+
+  const fetchCollection = async () => {
+    const db = await getDatabase()
+    const { documents } = await db.listDocuments(DATABASE_ID, COLLECTION_ID)
+    console.log('documents', documents)
+    setOwnedCards(documents as unknown as CollectionRow[])
+  }
+
   return (
     <ThemeProvider defaultTheme="system" storageKey="tcgpct-ui-theme">
       <Toaster />
       <Header user={user} setUser={setUser} />
       <Routes>
-        <Route path="/" element={<Overview />} />
+        <Route path="/" element={<Overview user={user} />} />
         <Route path="/verify" element={<Verify />} />
-        <Route path="/collection" element={<Collection user={user} />} />
+        <Route path="/collection" element={<Collection user={user} ownedCards={ownedCards} setOwnedCards={setOwnedCards} />} />
         <Route path="/pokedex" element={<Pokedex />} />
         <Route path="/trade" element={<Trade />} />
       </Routes>
--- frontend/src/pages/Overview.tsx
+++ frontend/src/pages/Overview.tsx
@@ -1,10 +1,30 @@
+import type { Models } from 'appwrite'
 import type { FC } from 'react'
 
-export const Overview: FC = () => {
+interface Props {
+  user: Models.User<Models.Preferences> | null
+}
+
+export const Overview: FC<Props> = ({ user }) => {
+  if (user) {
+    return (
+      <div className="grid grid-cols-4">
+        <h1 className="text-2xl font-bold mb-4 text-gray-900 dark:text-gray-100">Overview</h1>
+        <p className="mb-2 text-gray-700 dark:text-gray-300">Welcome to the TCGPCT!</p>
+        <p className="mb-2 text-gray-700 dark:text-gray-300">Here you can view your collection, trade with other users, and view the Pokedex.</p>
+        <p className="mb-2 text-gray-700 dark:text-gray-300">Click on the links above to get started!</p>
+      </div>
+    )
+  }
   return (
-    <div className="flex flex-col justify-center items-center m-40">
-      <h1 className="text-3xl font-bold">TCG Pocket Collection Tracker</h1>
-      <p className="mb-5">Work in progress, check back soon!</p>
-    </div>
+    <section className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+      <div className="mx-auto max-w-screen-md sm:text-center">
+        <h2 className="mb-4 text-3xl tracking-tight font-extrabold sm:text-4xl">Sign up to view your card statistics</h2>
+        <p className="mx-auto mb-8 max-w-2xl md:mb-12 sm:text-xl">To view your card statistics, please register or log in.</p>
+        <p className="mx-auto mb-8 max-w-2xl md:mb-12 sm:text-xl">
+          By registering, you can keep track of your collection, trade with other users, and access exclusive features.
+        </p>
+      </div>
+    </section>
   )
 }
