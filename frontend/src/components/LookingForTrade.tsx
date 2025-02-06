694a53722ae0d600e3991ed4a4efdf63b4f60565
SpyderHunter03 <SpyderHunter03@gmail.com>
2025-02-06 15:35:00 -0500

    Looking For Trade Implementation and Lint Fixes

---
+++ frontend/src/components/LookingForTrade.tsx
@@ -0,0 +1,109 @@
+import { createColumnHelper, getCoreRowModel, getGroupedRowModel, Row, useReactTable } from "@tanstack/react-table"
+import { useVirtualizer } from "@tanstack/react-virtual"
+import { useMemo, useRef } from "react"
+import type { Card } from '@/types'
+import type { Card as CardType } from '../types'
+import FancyCard from "./FancyCard"
+
+export function LookingForTrade({ cards }: { cards: CardType[] }) {
+  const columnHelper = createColumnHelper<CardType>()
+  const parentRef = useRef<HTMLDivElement>(null)
+
+  const columns = useMemo(() => {
+    return [
+      columnHelper.accessor('image', {
+        id: 'imageUrl',
+      }),
+      columnHelper.accessor('id', {
+        id: 'id',
+      }),
+      columnHelper.accessor('name', {
+        id: 'name',
+      }),
+      columnHelper.accessor('pack', {
+        id: 'pack',
+      }),
+    ]
+  }, [])
+
+  // Columns and data are defined in a stable reference, will not cause infinite loop!
+  const table = useReactTable({
+    columns,
+    data: cards,
+    enableGrouping: true,
+    getCoreRowModel: getCoreRowModel(),
+    getGroupedRowModel: getGroupedRowModel(),
+    initialState: {
+      grouping: ['pack'],
+    },
+  })
+  const groupedRows = table.getRowModel().rows
+
+  const groupedGridRows = groupedRows.map((groupRow) => {
+    const header = { type: 'header', row: groupRow }
+    const dataRows = groupRow.subRows.map((subRow) => ({ type: 'data', row: subRow }))
+
+    const gridRows = []
+    for (let i = 0; i < dataRows.length; i += 5) {
+      gridRows.push(dataRows.slice(i, i + 5))
+    }
+
+    return { header, gridRows }
+  })
+
+  const flattenedRows = groupedGridRows.flatMap((group) => [
+    { type: 'header', height: 45, data: group.header }, // Group header
+    ...group.gridRows.map((gridRow) => ({ type: 'gridRow', height: 269, data: gridRow })), // Grid rows
+  ])
+
+  const rowVirtualizer = useVirtualizer({
+    getScrollElement: () => parentRef.current,
+    count: flattenedRows.length,
+    estimateSize: (index) => (flattenedRows[index].type === 'header' ? 45 : 269) + 12,
+    overscan: 5,
+  })
+
+  return (
+    <div ref={parentRef} className="h-[calc(100vh-180px)] overflow-y-auto">
+      <div
+        style={{
+          height: `${rowVirtualizer.getTotalSize()}px`,
+          position: 'relative',
+        }}
+        className="w-full"
+      >
+        {rowVirtualizer.getVirtualItems().map((virtualRow) => {
+          const row = flattenedRows[virtualRow.index]
+          console.log("Row", row)
+          return (
+            <div
+              key={virtualRow.key}
+              style={{
+                position: 'absolute',
+                top: 0,
+                left: 0,
+                width: '100%',
+                height: `${virtualRow.size}px`,
+                transform: `translateY(${virtualRow.start}px)`,
+              }}
+            >
+              {row.type === 'header' ? (
+                <></>
+              ) : (
+                <div className="flex justify-center gap-5">
+                  {(row.data as { type: string; row: Row<Card> }[]).map(({ row: subRow }) => {
+                    return (
+                      <div className="flex flex-col items-center gap-y-4 w-fit border border-gray-700 p-4 rounded-lg shadow-md hover:shadow-lg transition duration-200 group">
+                        <FancyCard key={subRow.original.id} card={subRow.original} selected={true} setIsSelected={() => {}} />
+                      </div>
+                    );
+                  })}
+                </div>
+              )}
+            </div>
+          )
+        })}
+      </div>
+    </div>
+  )
+}
--- frontend/src/pages/Trade.tsx
+++ frontend/src/pages/Trade.tsx
@@ -1,3 +1,57 @@
+import { useEffect, useState } from "react";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { COLLECTION_ID, DATABASE_ID, getDatabase } from "@/lib/Auth";
+import { CollectionRow } from "@/types";
+import type { Card as CardType } from '../types'
+import A1 from '../../assets/cards/A1.json'
+import A1a from '../../assets/cards/A1a.json'
+import A2 from '../../assets/cards/A2.json'
+import PA from '../../assets/cards/P-A.json'
+import { LookingForTrade } from "@/components/LookingForTrade";
+
+const a1Cards: CardType[] = A1 as unknown as CardType[]
+const a2Cards: CardType[] = A2 as unknown as CardType[]
+const a1aCards: CardType[] = A1a as unknown as CardType[]
+const paCards: CardType[] = PA as unknown as CardType[]
+
 export function Trade() {
-  return <h1>Coming soon</h1>
+  const [ownedCards, setOwnedCards] = useState<CollectionRow[]>([])
+    
+  useEffect(() => {
+    fetchCollection()
+  }, [])
+
+  const fetchCollection = async () => {
+    const db = await getDatabase()
+    const { documents } = await db.listDocuments(DATABASE_ID, COLLECTION_ID)
+    console.log('documents', documents)
+    setOwnedCards(documents as unknown as CollectionRow[])
+  }
+  
+  const lookingForTradeCards = () => {
+    const allCards = [...a1Cards, ...a2Cards, ...a1aCards, ...paCards]
+    const missingCards = allCards.filter(ac => ownedCards.findIndex(oc => oc.card_id === ac.id) == -1)
+    return missingCards;
+  }
+
+  return (
+    <div className="flex flex-col gap-y-4 max-w-[900px] mx-auto">
+      <Tabs defaultValue="looking_for">
+        <TabsList className="m-auto mt-4 mb-8">
+          <TabsTrigger value="looking_for">Looking For</TabsTrigger>
+          <TabsTrigger value="for_trade">For Trade</TabsTrigger>
+          <TabsTrigger value="buying_tokens">Buying Tokens</TabsTrigger>
+        </TabsList>
+        <TabsContent value="looking_for">
+          <LookingForTrade cards={lookingForTradeCards()}/>
+        </TabsContent>
+        <TabsContent value="for_trade">
+          <span>For Trade</span>
+        </TabsContent>
+        <TabsContent value="buying_tokens">
+          <span>Buying Tokens</span>
+        </TabsContent>
+      </Tabs>
+    </div>
+  );
 }
