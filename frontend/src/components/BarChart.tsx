ae95f56f1874a4ef9069467f6be27abc27b1d9a9
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2025-02-06 14:12:37 +0100

    ✨ feat: rename PercentageBarChart to BarChart and add footer support; introduce new PieChart component

--- frontend/src/components/BarChart.tsx
+++ frontend/src/components/BarChart.tsx
@@ -1,7 +1,7 @@
 'use client'
-import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
 import { type ChartConfig, ChartContainer, ChartTooltip, ChartTooltipContent } from '@/components/ui/chart'
-import { Bar, BarChart, CartesianGrid, Rectangle, XAxis } from 'recharts'
+import { Bar, BarChart, CartesianGrid } from 'recharts'
 
 //Example of a percentage bar chart configuration
 /* const chartData = [
@@ -28,6 +28,7 @@ const chartConfig = {
 interface PercentageBarChartProps {
   data: { packName: string; percentage: number; fill: string }[]
   config: ChartConfig
+  footer?: string
 }
 
 interface CustomTooltipContentProps {
@@ -38,45 +39,33 @@ interface CustomTooltipContentProps {
 const CustomTooltipContent = (props: CustomTooltipContentProps) => {
   const { payload, active } = props
   if (active && payload && payload.length) {
-    const newPayload = payload.map((entry) => ({
-      ...entry,
-      value: `${entry.value * 100}%`,
-    }))
+    const newPayload = payload.map((entry) => {
+      return {
+        ...entry,
+        value: `${entry.value * 100}%`,
+      }
+    })
     return <ChartTooltipContent {...props} payload={newPayload} />
   }
   return null
 }
 
-export function PercentageBarChart({ data, config }: PercentageBarChartProps) {
+export function BarChartComponent({ data, config, footer }: PercentageBarChartProps) {
   return (
     <Card>
-      <CardHeader>
+      <CardHeader className="text-center text-balance">
         <CardTitle>Probability getting new card</CardTitle>
       </CardHeader>
       <CardContent>
         <ChartContainer config={config}>
           <BarChart accessibilityLayer data={data}>
             <CartesianGrid vertical={false} />
-            <XAxis
-              dataKey="packName"
-              tickLine={false}
-              tickMargin={10}
-              axisLine={false}
-              tickFormatter={(value) => config[value as keyof typeof config]?.label || value}
-            />
             <ChartTooltip cursor={false} content={<CustomTooltipContent payload={[]} active={false} />} />
-            <Bar
-              dataKey="percentage"
-              strokeWidth={2}
-              radius={8}
-              activeIndex={2}
-              activeBar={({ ...props }) => {
-                return <Rectangle {...props} fillOpacity={0.8} stroke={props.payload.fill} strokeDasharray={4} strokeDashoffset={4} />
-              }}
-            />
+            <Bar dataKey="percentage" strokeWidth={2} radius={8} />
           </BarChart>
         </ChartContainer>
       </CardContent>
+      <CardFooter className="flex-col items-center gap-2">{footer}</CardFooter>
     </Card>
   )
 }
