00d332bba78122f8b4c563dff3d9a2e4d0b7bff5
marcel panse <marcel.panse@gmail.com>
2025-02-09 11:25:58 +0100

    fix reverted login logic

--- frontend/src/pages/verify/Verify.tsx
+++ frontend/src/pages/verify/Verify.tsx
@@ -1,17 +1,20 @@
 import { login } from '@/lib/Auth.ts'
-import { Suspense, use, useEffect } from 'react'
+import { useEffect } from 'react'
 import { ErrorBoundary } from 'react-error-boundary'
-import { useNavigate } from 'react-router'
 
 function Verify() {
-  const user = use(login())
-  const navigate = useNavigate()
-
   useEffect(() => {
-    if (user) {
-      navigate('/')
-    }
-  }, [user])
+    login()
+      .then((user) => {
+        if (user) {
+          console.log('logged in')
+          window.location.href = '/'
+        } else {
+          throw new Error('Error logging in!')
+        }
+      })
+      .catch(console.error)
+  }, [])
 
   return <span>Logged in! Redirecting...</span>
 }
@@ -21,9 +24,9 @@ function VerifyContainer() {
     <div className="m-40 flex flex-col items-center justify-center">
       <h5 className="mb-5">
         <ErrorBoundary fallback={<span className="text-red-500">Error logging in!</span>}>
-          <Suspense fallback={<span>Logging in...</span>}>
-            <Verify />
-          </Suspense>
+          {/*<Suspense fallback={<span>Logging in...</span>}>*/}
+          <Verify />
+          {/*</Suspense>*/}
         </ErrorBoundary>
       </h5>
     </div>
