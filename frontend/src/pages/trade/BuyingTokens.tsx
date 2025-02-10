363a52bc727401c8236ea189cea93d3656663ee3
Jason Blee <jasonblee97@gmail.com>
2025-02-10 23:51:56 +0000

    fix missed amount_owned setup

--- frontend/src/pages/trade/BuyingTokens.tsx
+++ frontend/src/pages/trade/BuyingTokens.tsx
@@ -17,7 +17,7 @@ export function BuyingTokens() {
         .filter((ac) => forTradeCards.findIndex((oc) => oc.card_id === ac.card_id) > -1)
         .map((ac) => ({
           ...ac,
-          amount_owned: forTradeCards.find((oc) => oc.card_id === ac.card_id)?.amount_owned,
+          amount_owned: forTradeCards.find((oc) => oc.card_id === ac.card_id)?.amount_owned || 0,
         })),
     [forTradeCards],
   )
