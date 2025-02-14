6a67086255124b0dfa1b9387f7837aef1926c2cb
Ruben Javier Bacho Alarcon <rjbachoa01@educastillalamancha.es>
2025-02-14 19:04:58 +0100

    feat: add Carousel component and update Overview layout for mobile

--- frontend/src/pages/overview/Overview.tsx
+++ frontend/src/pages/overview/Overview.tsx
@@ -36,34 +36,37 @@ function Overview() {
   }, [ownedCards, rarityFilter])
 
   return (
-    <main className="fade-in-up mx-auto min-h-screen max-w-7xl p-8">
-      <div className="mb-8">
-        <RarityFilter setRarityFilter={setRarityFilter} />
-      </div>
-
-      <section className="grid grid-cols-8 gap-6">
-        <div className="col-span-8 flex aspect-square h-full w-full flex-col items-center justify-center rounded-4xl border-2 border-gray-200 border-solid p-8 lg:col-span-2">
-          <h2 className="mb-2 text-center text-2xl">You have</h2>
-          <h1 className="mb-3 text-balance text-center font-semibold text-7xl">{CardsDB.getNrOfCardsOwned({ ownedCards, rarityFilter })}</h1>
-          <h2 className="text-balance text-center text-2xl">out of {CardsDB.getTotalNrOfCards({ rarityFilter })} unique cards</h2>
-        </div>
-        <GradientCard
-          title={highestProbabilityPack?.packName || ''}
-          packNames="all"
-          percentage={highestProbabilityPack?.percentage || 0}
-          className="col-span-8 lg:col-span-4 col-start-1 lg:col-start-3"
-          backgroundColor={highestProbabilityPack?.fill}
-        />
-        <div className="col-span-8 flex aspect-square h-full w-full flex-col items-center justify-center rounded-4xl border-2 border-gray-200 border-solid p-8 opacity-100 lg:col-span-2">
-          <h2 className="mb-2 text-center text-2xl">You have</h2>
-          <h1 className="mb-3 overflow-hidden truncate whitespace-nowrap text-balance text-center font-semibold text-7xl">{ownedCardsCount}</h1>
-          <h2 className="text-balance text-center text-2xl">cards in Total</h2>
+    <main className="fade-in-up">
+      <article className="mx-auto max-w-7xl px-8">
+        <div className="mb-8">
+          <RarityFilter setRarityFilter={setRarityFilter} />
         </div>
 
+        <section className="grid grid-cols-8 gap-6">
+          <div className="col-span-8 flex aspect-square h-full w-full flex-col items-center justify-center rounded-4xl border-2 border-gray-200 border-solid p-8 lg:col-span-2">
+            <h2 className="mb-2 text-center text-2xl">You have</h2>
+            <h1 className="mb-3 text-balance text-center font-semibold text-7xl">{CardsDB.getNrOfCardsOwned({ ownedCards, rarityFilter })}</h1>
+            <h2 className="text-balance text-center text-2xl">out of {CardsDB.getTotalNrOfCards({ rarityFilter })} unique cards</h2>
+          </div>
+          <GradientCard
+            title={highestProbabilityPack?.packName || ''}
+            packNames="all"
+            percentage={highestProbabilityPack?.percentage || 0}
+            className="col-span-8 lg:col-span-4 col-start-1 lg:col-start-3"
+            backgroundColor={highestProbabilityPack?.fill}
+          />
+          <div className="col-span-8 flex aspect-square h-full w-full flex-col items-center justify-center rounded-4xl border-2 border-gray-200 border-solid p-8 opacity-100 lg:col-span-2">
+            <h2 className="mb-2 text-center text-2xl">You have</h2>
+            <h1 className="mb-3 overflow-hidden truncate whitespace-nowrap text-balance text-center font-semibold text-7xl">{ownedCardsCount}</h1>
+            <h2 className="text-balance text-center text-2xl">cards in Total</h2>
+          </div>
+        </section>
+      </article>
+      <article className="mx-auto min-h-screen max-w-7xl sm:p-6 p-0 grid grid-cols-8 gap-6">
         {CardsDB.expansions.map((expansion) => (
           <ExpansionOverview key={expansion.id} expansion={expansion} rarityFilter={rarityFilter} />
         ))}
-      </section>
+      </article>
     </main>
   )
 }
