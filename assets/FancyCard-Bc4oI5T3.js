d98ebe2cea56d4e3aeb87401245676489c057ef9
marcelpanse <marcel.panse@gmail.com>
2025-02-15 14:44:44 +0000

    Deploying to gh-pages from @ marcelpanse/tcg-pocket-collection-tracker@7f830e8a74fa36a36ceace32e2cfc8ed0bf6f4a1 🚀

--- assets/FancyCard-Bc4oI5T3.js
+++ assets/FancyCard-Bc4oI5T3.js
@@ -1,4 +1,4 @@
-import{r as $,n as $e,R as re,j as se}from"./index-4xgRfaJU.js";function le(){const{innerWidth:e,innerHeight:i}=window;return{width:e,height:i}}function xt(){const[e,i]=$.useState(le());return $.useEffect(()=>{function t(){i(le())}return window.addEventListener("resize",t),()=>window.removeEventListener("resize",t)},[]),e}/**
+import{r as $,n as $e,R as re,j as se}from"./index-B1V_cZYA.js";function le(){const{innerWidth:e,innerHeight:i}=window;return{width:e,height:i}}function xt(){const[e,i]=$.useState(le());return $.useEffect(()=>{function t(){i(le())}return window.addEventListener("resize",t),()=>window.removeEventListener("resize",t)},[]),e}/**
    * table-core
    *
    * Copyright (c) TanStack
