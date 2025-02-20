a19c1c8f1bcff0437553872c2c48506747244764
rexwithluv <aaronaax@gmail.com>
2025-02-20 20:01:52 +0100

    feat: wrap application in I18nextProvider for lang support

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,9 @@
 import { StrictMode, Suspense } from 'react'
 import { createRoot } from 'react-dom/client'
+import { I18nextProvider } from 'react-i18next'
 import { HashRouter } from 'react-router'
 import App from './App.tsx'
+import i18n from './i18n.tsx'
 import './index.css'
 
 const root = document.getElementById('root')
@@ -12,9 +14,11 @@ if (!root) {
 createRoot(root).render(
   <StrictMode>
     <Suspense fallback={<div>Loading...</div>}>
-      <HashRouter>
-        <App />
-      </HashRouter>
+      <I18nextProvider i18n={i18n}>
+        <HashRouter>
+          <App />
+        </HashRouter>
+      </I18nextProvider>
     </Suspense>
   </StrictMode>,
 )
