07b8bba380bd152cb4218514385f8b87388d3849
rexwithluv <aaronaax@gmail.com>
2025-02-22 22:58:39 +0100

    feat: edit i18n.tsx to load files from namespace

--- frontend/src/i18n.tsx
+++ frontend/src/i18n.tsx
@@ -1,15 +1,17 @@
 import i18n from 'i18next'
+import Backend from 'i18next-http-backend'
 import { initReactI18next } from 'react-i18next'
-import en from './locales/en/translation.json'
-import es from './locales/es/translation.json'
 
-i18n.use(initReactI18next).init({
-  resources: {
-    en: { translation: en },
-    es: { translation: es },
-  },
-  lng: 'en', // default language
-  interpolation: { escapeValue: false },
-})
+i18n
+  .use(Backend)
+  .use(initReactI18next)
+  .init({
+    lng: 'en', // default language
+    interpolation: { escapeValue: false },
+    fallbackLng: 'en',
+    backend: {
+      loadPath: '/src/locales/{{lng}}/{{ns}}.json',
+    },
+  })
 
 export default i18n
