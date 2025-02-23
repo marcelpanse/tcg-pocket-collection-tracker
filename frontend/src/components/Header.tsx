d87b174bfe15aa3b6f36d8eb80c32623f08c3fe2
rexwithluv <114663850+rexwithluv@users.noreply.github.com>
2025-02-23 21:25:54 +0100

    Translated the main menus (#208)
    * chore: added 'react-i18next' 'i18next' packages
    
    * chore: created lang files
    
    * refactor: created dropdown icon to select lang
    
    * feat: add i18n hook and initialize i18next for language support
    
    * chore: update pnpm-lock.yaml to include i18next and react-i18next dependencies
    
    * chore: update packageManager version to pnpm@10.4.1
    
    * fix: update paths and imports for translation files
    
    * feat: wrap application in I18nextProvider for lang support
    
    * chore: update translation.json to include header
    
    * chore: add Spanish for header translations
    
    * feat: update Header component to use translation i18n
    
    * feat: added i18n-http-backend to load multiple .json files
    
    * feat: edit i18n.tsx to load files from namespace
    
    * chore: changed the names for .json files
    
    * refactor: now it will take only its corresponding file
    
    * translate: ownedFilter component to Spanish
    
    * style: increase width of OwnedFilter tabs for spanish lang
    
    * translate: gradientCard overview component to spanish
    
    * translate: overview page to spanish
    
    * translate: community page to spanish
    
    * translate: completeProgress overview component to spanish
    
    * translate: expansionOverview overview component to spanish
    
    * translate: editProfile component to spanish
    
    * translate: update gradient-card and overview .json files to organize
    
    * fix: i18n namespaces
    
    * translate: rarityFilter component to spanish
    
    * translation: hamburguerMenu component to spanish
    
    * translate: login component to spanish

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -14,12 +14,16 @@ import {
 import { NavigationMenu, NavigationMenuLink, NavigationMenuList } from '@/components/ui/navigation-menu.tsx'
 import { logout } from '@/lib/Auth.ts'
 import { UserContext } from '@/lib/context/UserContext.ts'
-import { LogOut, UserRoundPen } from 'lucide-react'
+import { Globe, LogOut, UserRoundPen } from 'lucide-react'
 import { use } from 'react'
+import { useTranslation } from 'react-i18next'
 import { Link } from 'react-router'
 
 export function Header() {
   const { user, setUser, isLoginDialogOpen, setIsLoginDialogOpen, setIsProfileDialogOpen } = use(UserContext)
+  const { t, i18n } = useTranslation('header')
+  const changeLanguage = (lng: string) => i18n.changeLanguage(lng)
+
   return (
     <>
       <header className="flex h-20 w-full shrink-0 flex-wrap items-center px-4 md:px-6">
@@ -28,24 +32,37 @@ export function Header() {
           <NavigationMenuList>
             <NavigationMenuLink asChild>
               <Link to="/">
-                <Button variant="ghost">Overview</Button>
+                <Button variant="ghost">{t('overview')}</Button>
               </Link>
             </NavigationMenuLink>
             <NavigationMenuLink asChild>
               <Link to="/collection">
-                <Button variant="ghost">Collection</Button>
+                <Button variant="ghost">{t('collection')}</Button>
               </Link>
             </NavigationMenuLink>
             <NavigationMenuLink asChild className="hidden sm:block">
               <Link to="/trade">
-                <Button variant="ghost">Trade</Button>
+                <Button variant="ghost">{t('trade')}</Button>
               </Link>
             </NavigationMenuLink>
           </NavigationMenuList>
         </NavigationMenu>
         <div className="items-center gap-2 hidden sm:flex">
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Button variant="ghost" size="icon">
+                <Globe />
+              </Button>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent className="w-56">
+              <DropdownMenuLabel>{t('selectLanguage')}</DropdownMenuLabel>
+              <DropdownMenuSeparator />
+              <DropdownMenuItem onClick={() => changeLanguage('es')}>{t('languages.es')}</DropdownMenuItem>
+              <DropdownMenuItem onClick={() => changeLanguage('en')}>{t('languages.en')}</DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
           <Link to="/community">
-            <Button variant="ghost">Community</Button>
+            <Button variant="ghost">{t('community')}</Button>
           </Link>
 
           {user ? (
@@ -56,9 +73,9 @@ export function Header() {
                 </Button>
               </DropdownMenuTrigger>
               <DropdownMenuContent className="w-56">
-                <DropdownMenuLabel>My account</DropdownMenuLabel>
+                <DropdownMenuLabel>{t('myAccount')}</DropdownMenuLabel>
                 <DropdownMenuSeparator />
-                <DropdownMenuItem onClick={() => setIsProfileDialogOpen(true)}>Edit profile</DropdownMenuItem>
+                <DropdownMenuItem onClick={() => setIsProfileDialogOpen(true)}>{t('editProfile')}</DropdownMenuItem>
                 <DropdownMenuSeparator />
                 <DropdownMenuGroup>
                   <DropdownMenuSeparator />
@@ -68,7 +85,7 @@ export function Header() {
                       setUser(null)
                     }}
                   >
-                    Log out
+                    {t('logOut')}
                     <LogOut />
                   </DropdownMenuItem>
                 </DropdownMenuGroup>
@@ -77,11 +94,11 @@ export function Header() {
           ) : (
             <Dialog open={isLoginDialogOpen} onOpenChange={setIsLoginDialogOpen}>
               <DialogTrigger asChild>
-                <Button>Login</Button>
+                <Button>{t('login')}</Button>
               </DialogTrigger>
               <DialogContent className="border-2 border-slate-600 shadow-none">
                 <DialogHeader>
-                  <DialogTitle>Login / Sign up</DialogTitle>
+                  <DialogTitle>{t('signUp')}</DialogTitle>
                 </DialogHeader>
                 <Login />
               </DialogContent>
