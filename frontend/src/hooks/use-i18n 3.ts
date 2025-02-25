34bcdd8ac7688ce35c505de2a5f76e944b10a0a0
Lucian <medrihanlucian@gmail.com>
2025-02-25 11:57:04 +0100

    duplicated files generated by conflicts

--- frontend/src/hooks/use-i18n 3.ts
+++
@@ -1,7 +0,0 @@
-import { useTranslation } from 'react-i18next'
-
-export const useI18n = () => {
-  const { t, i18n } = useTranslation()
-  const changeLanguage = (lng: string) => i18n.changeLanguage(lng)
-  return { t, changeLanguage }
-}
