fb0064691df39b4b22cd9dd1486257fc453e3501
marcelpanse <marcel.panse@gmail.com>
2025-05-27 16:10:23 +0000

    Deploying to gh-pages from @ marcelpanse/tcg-pocket-collection-tracker@caa1ea65ce1816febc4249d640d84c37118c3397 🚀

--- assets/CardsTable-Dpe-1ZzT.js
+++ assets/CardsTable-Dpe-1ZzT.js
@@ -1,4 +1,4 @@
-import{r as $,a6 as $e,u as Ie,j as V}from"./index-Dbm1Jzd3.js";import{C as ye}from"./Card-t8ALS56p.js";function le(){const{innerWidth:e,innerHeight:i}=window;return{width:e,height:i}}function Ve(){const[e,i]=$.useState(le());return $.useEffect(()=>{function t(){i(le())}return window.addEventListener("resize",t),()=>window.removeEventListener("resize",t)},[]),e}/**
+import{r as $,aA as $e,u as Ie,j as V}from"./index-C_mAOLwK.js";import{C as ye}from"./Card-DFazbCeL.js";function le(){const{innerWidth:e,innerHeight:i}=window;return{width:e,height:i}}function Ve(){const[e,i]=$.useState(le());return $.useEffect(()=>{function t(){i(le())}return window.addEventListener("resize",t),()=>window.removeEventListener("resize",t)},[]),e}/**
    * table-core
    *
    * Copyright (c) TanStack
