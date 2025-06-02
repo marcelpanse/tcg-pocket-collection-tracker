a6ff07bb2078c42b4ddf22aed91626ac7a47c686
marcelpanse <marcel.panse@gmail.com>
2025-06-02 09:42:01 +0000

    Deploying to gh-pages from @ marcelpanse/tcg-pocket-collection-tracker@2cb50d06f04008ea062ad8b0f96d0ccd1f7a0db3 🚀

--- assets/CardDetail-CnfCZs6W.js
+++ assets/CardDetail-CnfCZs6W.js
@@ -1,4 +1,4 @@
-import{r as y,aB as Ie,u as fe,j as C,H as Ve,K as Pe,S as Ee,L as ze,N as De,O as Oe,s as Ae,t as He,Q as Ge,aA as Le}from"./index-CKP-uMfZ.js";import{C as pe}from"./Card-BMV3Z61v.js";function le(){const{innerWidth:e,innerHeight:i}=window;return{width:e,height:i}}function Te(){const[e,i]=y.useState(le());return y.useEffect(()=>{function t(){i(le())}return window.addEventListener("resize",t),()=>window.removeEventListener("resize",t)},[]),e}/**
+import{r as y,aB as Ie,u as fe,j as C,H as Ve,K as Pe,S as Ee,L as ze,N as De,O as Oe,s as Ae,t as He,Q as Ge,aA as Le}from"./index-BRWSmaXV.js";import{C as pe}from"./Card-CXNLMqfM.js";function le(){const{innerWidth:e,innerHeight:i}=window;return{width:e,height:i}}function Te(){const[e,i]=y.useState(le());return y.useEffect(()=>{function t(){i(le())}return window.addEventListener("resize",t),()=>window.removeEventListener("resize",t)},[]),e}/**
    * table-core
    *
    * Copyright (c) TanStack
