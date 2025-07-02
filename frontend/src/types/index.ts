bfe3f6e27a3f62f48ac569fb7d24b69398dd8d86
StephenAloche <stephen.aloche@gmail.com>
2025-07-03 00:04:49 +0200

    add filter cardType

--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -3,7 +3,10 @@ export type ExpansionId = (typeof expansionIds)[number]
 
 const rarities = ['◊', '◊◊', '◊◊◊', '◊◊◊◊', '☆', '☆☆', '☆☆☆', '✵', '✵✵', 'Crown Rare', 'P', ''] as const
 
+export const cardTypes = ['colorless', 'fire', 'water', 'grass', 'lightning', 'psychic', 'fighting', 'darkness', 'metal', 'trainer', ''] as const
+
 export type Rarity = (typeof rarities)[number]
+export type CardType = (typeof cardTypes)[number]
 
 export interface AccountRow {
   $id: string
