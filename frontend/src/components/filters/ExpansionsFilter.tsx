d4a4cce6dd4ae512b41dbc789c1ad443c402c259
marcel panse <marcel.panse@gmail.com>
2025-07-28 19:35:25 +0200

    fix: text sizes

--- frontend/src/components/filters/ExpansionsFilter.tsx
+++ frontend/src/components/filters/ExpansionsFilter.tsx
@@ -11,8 +11,8 @@ const ExpansionsFilter: FC<Props> = ({ value, onChange }) => {
 
   return (
     <label className="flex items-baseline justify-between gap-5 px-3 my-auto border-1 border-neutral-700 rounded-md p-1 bg-neutral-800 text-neutral-400">
-      <h2 className="h-full">{t('expansion')}</h2>
-      <select value={value} onChange={(e) => onChange(e.target.value)} className="min-h-[2rem]">
+      <div className="text-sm">{t('expansion')}</div>
+      <select value={value} onChange={(e) => onChange(e.target.value)} className="min-h-[27px] text-sm">
         <option value="all">{t('all')}</option>
         {expansions.map((expansion) => (
           <option key={expansion.id} value={expansion.id}>
