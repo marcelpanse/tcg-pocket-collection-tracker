8429a44567ff69611652e742056fe0ae32b711c2
marcel panse <marcel.panse@gmail.com>
2025-10-15 16:30:36 +0200

    finished migration

--- scripts/sql/internal_cards_migration.sql
+++ scripts/sql/internal_cards_migration.sql
@@ -6,8 +6,13 @@ UPDATE collection c
 SET internal_id = cl.internal_id
 FROM cards_list cl
 WHERE c.card_id = cl.card_id
-  AND c.email = 'marcel.panse@gmail.com'
-  AND c.internal_id IS NULL;
+  AND c.internal_id IS NULL
+  AND c.ctid IN (
+    SELECT c2.ctid
+    FROM collection c2
+    WHERE c2.internal_id IS NULL
+    LIMIT 50000
+);
 
 INSERT INTO card_amounts (internal_id, email, amount_owned, created_at, updated_at)
 SELECT
@@ -18,6 +23,5 @@ SELECT
     MAX(COALESCE(c.updated_at, NOW())) as updated_at
 FROM collection c
          INNER JOIN cards_list cl ON c.card_id = cl.card_id
-WHERE c.email = 'marcel.panse@gmail.com'
 GROUP BY cl.internal_id, c.email
 ON CONFLICT (internal_id, email) DO UPDATE SET amount_owned = EXCLUDED.amount_owned;
