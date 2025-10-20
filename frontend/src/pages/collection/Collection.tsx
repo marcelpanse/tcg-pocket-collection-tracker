7cd7e4a3a79e74ae185039166447b2255def4b90
Grzegorz Kwacz <gkwacz@hotmail.com>
2025-10-20 10:43:01 +0200

    Add friendId to path

--- frontend/src/pages/collection/Collection.tsx
+++ frontend/src/pages/collection/Collection.tsx
@@ -1,61 +1,90 @@
 import { Siren } from 'lucide-react'
+import { useEffect } from 'react'
 import { useTranslation } from 'react-i18next'
 import { useNavigate, useParams } from 'react-router'
 import { Alert, AlertDescription, AlertTitle } from '@/components/ui/alert'
 import { Button } from '@/components/ui/button'
-import { usePublicAccount } from '@/services/account/useAccount.ts'
+import { useAccount, usePublicAccount } from '@/services/account/useAccount.ts'
 import { useCollection, usePublicCollection } from '@/services/collection/useCollection'
 import type { CollectionRow } from '@/types'
 import CollectionCards from './CollectionCards'
 
-function Collection() {
-  const { t } = useTranslation(['pages/collection', 'common'])
+function OwnCollection() {
+  const { t } = useTranslation('common')
+  const { data: cards = new Map<number, CollectionRow>(), isLoading } = useCollection()
+  if (isLoading) {
+    return <p className="text-xl text-center py-8">{t('common:loading')}</p>
+  }
+  return (
+    <div className="flex flex-col gap-y-1 mx-auto max-w-[900px]">
+      <CollectionCards cards={cards} isPublic={false} extraOffset={20} />
+    </div>
+  )
+}
 
-  const { friendId } = useParams()
+function FriendCollection({ friendId }: { friendId: string }) {
   const navigate = useNavigate()
+  const { t } = useTranslation(['pages/collection', 'common'])
+  const { data: account, isLoading: isLoadingAccount } = usePublicAccount(friendId)
+  const { data: cards, isLoading: isLoadingCards } = usePublicCollection(friendId)
 
-  const { data: friendAccount } = usePublicAccount(friendId)
-  const { data: friendCards } = usePublicCollection(friendId)
-
-  const { data: ownedCards = new Map<number, CollectionRow>(), isLoading } = useCollection()
-
-  if (friendId) {
-    if (friendAccount === null) {
-      return <p className="text-xl text-center py-8">{t('common:noAccount')}</p>
-    } else if (friendCards === null) {
-      return <p className="text-xl text-center py-8">Not a public account</p>
-    } else if (friendCards === undefined || friendAccount === undefined) {
-      return <p className="text-xl text-center py-8">{t('common:loading')}...</p>
-    }
-
-    return (
-      <div className="flex flex-col gap-y-1 mx-auto max-w-[900px]">
-        <Alert className="mb-4 border-1 border-neutral-700 shadow-none">
-          <Siren className="h-4 w-4" />
-          <AlertTitle>{t('publicCollectionTitle', { username: friendAccount?.username })}</AlertTitle>
-          <AlertDescription>
-            <div className="flex items-center">
-              {t('publicCollectionDescription')}
-              <Button className="mb-4" onClick={() => navigate(`/trade/${friendAccount?.friend_id}`)}>
-                {t('showPossibleTrades')}
-              </Button>
-            </div>
-          </AlertDescription>
-        </Alert>
-        <CollectionCards cards={friendCards} isPublic={true} extraOffset={136} />
-      </div>
-    )
+  if (isLoadingAccount || isLoadingCards) {
+    return <p className="text-xl text-center py-8">{t('common:loading')}</p>
+  }
+  if (account === undefined || cards === undefined) {
+    return <p className="text-xl text-center py-8">Something went wrong</p>
   }
 
-  if (isLoading) {
-    return <p className="text-xl text-center py-8">{t('common:loading')}...</p>
+  if (account === null) {
+    return <p className="text-xl text-center py-8">{t('common:noAccount')}</p>
+  }
+  if (cards === null) {
+    return <p className="text-xl text-center py-8">Not a public account</p>
   }
 
   return (
     <div className="flex flex-col gap-y-1 mx-auto max-w-[900px]">
-      <CollectionCards cards={ownedCards} isPublic={false} extraOffset={20} />
+      <Alert className="mb-1 border-1 border-neutral-700 shadow-none">
+        <Siren className="h-4 w-4" />
+        <AlertTitle>{t('publicCollectionTitle', { username: account.username })}</AlertTitle>
+        <AlertDescription>
+          <div className="flex items-center">
+            {t('publicCollectionDescription')}
+            <Button className="mb-4" onClick={() => navigate(`/trade/${friendId}`)}>
+              {t('showPossibleTrades')}
+            </Button>
+          </div>
+        </AlertDescription>
+      </Alert>
+      <CollectionCards cards={cards} isPublic={true} extraOffset={124} />
     </div>
   )
+}
+
+function Collection() {
+  const { t } = useTranslation(['pages/collection', 'common'])
+  const navigate = useNavigate()
+
+  const { friendId: rawFriendId } = useParams()
+  const { data: account, isLoading: isLoadingAccount } = useAccount()
+
+  useEffect(() => {
+    if (!isLoadingAccount && !rawFriendId && account?.friend_id) {
+      navigate(`/collection/${account.friend_id}`, { replace: true })
+    }
+  }, [isLoadingAccount, rawFriendId, account?.friend_id, navigate])
+
+  if (isLoadingAccount) {
+    return <p className="text-xl text-center py-8">{t('common:loading')}</p>
+  }
+
+  const friendId = rawFriendId ?? account?.friend_id
+
+  if (friendId && friendId !== account?.friend_id) {
+    return <FriendCollection friendId={friendId} />
+  } else {
+    return <OwnCollection />
+  }
 }
 
 export default Collection
