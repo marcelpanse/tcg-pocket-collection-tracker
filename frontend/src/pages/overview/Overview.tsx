0e278c4bd4e7bf84c016bce81a2473bd66d2c77c
Grzegorz Kwacz <gkwacz@hotmail.com>
2025-11-03 16:28:45 +0100

    Reorder expansions

--- frontend/src/pages/overview/Overview.tsx
+++ frontend/src/pages/overview/Overview.tsx
@@ -32,7 +32,7 @@ function Overview() {
 
   const [collectionCount, setCollectionCount] = useState('')
   const [usersCount, setUsersCount] = useState('')
-  const [expansionFilter, setExpansionFilter] = useState<ExpansionOption>(expansionOptions[expansionOptions.length - 3])
+  const [expansionFilter, setExpansionFilter] = useState<ExpansionOption>(expansionOptions[1])
 
   const ownedCardsCount = useMemo(() => {
     let total = 0
