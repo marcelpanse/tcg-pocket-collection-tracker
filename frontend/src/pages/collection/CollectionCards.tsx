317bd2fa18237cc38fbc4953bb76cb0ec3e4728e
Grzegorz Kwacz <gkwacz@hotmail.com>
2025-11-11 11:58:09 +0100

    move `CollectionFiltersPanel` to a separate file

--- frontend/src/pages/collection/CollectionCards.tsx
+++ frontend/src/pages/collection/CollectionCards.tsx
@@ -1,14 +1,9 @@
-import { CircleAlert } from 'lucide-react'
 import { type ReactNode, useMemo, useState } from 'react'
-import { useTranslation } from 'react-i18next'
 import { useMediaQuery } from 'react-responsive'
-import { useNavigate, useSearchParams } from 'react-router'
-import { Tooltip } from 'react-tooltip'
+import { useSearchParams } from 'react-router'
 import { CardsTable } from '@/components/CardsTable.tsx'
-import FilterPanel from '@/components/FiltersPanel'
 import { Button } from '@/components/ui/button'
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet'
-import { toast } from '@/hooks/use-toast'
 import {
   type CardTypeOption,
   cardTypeOptions,
@@ -19,9 +14,8 @@ import {
   ownedOptions,
   sortByOptions,
 } from '@/lib/filters'
-import { useProfileDialog } from '@/services/account/useAccount'
-import { useCollection } from '@/services/collection/useCollection'
-import { type Card, type CollectionRow, type Rarity, rarities } from '@/types'
+import { type CollectionRow, type Rarity, rarities } from '@/types'
+import CollectionFiltersPanel from './FiltersPanel'
 
 const numberParser = (s: string) => {
   const x = Number(s)
@@ -43,81 +37,6 @@ const filterParsers: { [K in keyof FiltersAll]: (s: string) => Filters[K] | unde
   maxNumber: numberParser,
   deckbuildingMode: boolParser,
   allTextSearch: boolParser,
-}
-
-interface FiltersSidePanelProps {
-  children: ReactNode
-  cards: Card[]
-  share?: boolean // undefined => disable, false => open settings, true => copy link
-  isPublic: boolean
-}
-
-function FiltersSidePanel({ children, cards, share, isPublic }: FiltersSidePanelProps) {
-  const { t } = useTranslation('pages/collection')
-  const navigate = useNavigate()
-  const { setIsProfileDialogOpen } = useProfileDialog()
-  const { data: ownedCards = new Map<number, CollectionRow>() } = useCollection()
-
-  async function onShare() {
-    if (!share) {
-      setIsProfileDialogOpen(true)
-    } else {
-      // @ts-expect-error: Experimental api https://developer.mozilla.org/en-US/docs/Web/API/NavigatorUAData/mobile
-      if (navigator.share && (navigator.userAgentData?.mobile || /Mobi|Android/i.test(navigator.userAgent))) {
-        await navigator.share({ title: 'My Pokemon TCG Pocket collection', url: window.location.href })
-      } else {
-        await navigator.clipboard.writeText(window.location.href)
-        toast({ title: 'Copied to clipboard', variant: 'default' })
-      }
-    }
-  }
-
-  const totalOwned = useMemo(() => {
-    let total = 0
-    const uniqueCardsByCardId = new Set<number>()
-    for (const card of cards) {
-      if (!uniqueCardsByCardId.has(card.internal_id)) {
-        total += card.amount_owned || 0
-        uniqueCardsByCardId.add(card.internal_id)
-      }
-    }
-    return total
-  }, [cards])
-
-  const mewCardOwned = useMemo(() => {
-    return Boolean((ownedCards?.get(83654)?.amount_owned ?? 0) > 0)
-  }, [cards])
-
-  return (
-    <div className="flex flex-col h-fit gap-2">
-      <small className="flex gap-2">
-        {t('stats.summary', {
-          selected: cards.length,
-          uniquesOwned: cards.filter((card) => Boolean(card.collected)).length,
-          totalOwned,
-        })}
-        {mewCardOwned && (
-          <>
-            <Tooltip id="mewCardOwned" className="text-start max-w-72" clickable={true} />
-            <CircleAlert className="h-5 w-5" data-tooltip-id="mewCardOwned" data-tooltip-content={t('stats.mewCardOwned')} />
-          </>
-        )}
-      </small>
-      {children}
-      <div className="flex flex-col mt-4 gap-2">
-        {share !== undefined && (
-          <Button variant="outline" onClick={onShare}>
-            {t('filters.share')}
-          </Button>
-        )}
-        {!isPublic && (
-          <Button variant="outline" onClick={() => navigate('/collection/missions')}>
-            {t('goToMissions')}
-          </Button>
-        )}
-      </div>
-    </div>
-  )
 }
 
 interface Props {
@@ -192,16 +111,26 @@ export default function CollectionCards({ children, cards, isPublic, share }: Pr
             <SheetHeader>
               <SheetTitle>Filters</SheetTitle>
             </SheetHeader>
-            <FiltersSidePanel cards={filteredCards} share={share} isPublic={isPublic}>
-              <FilterPanel className="flex flex-col gap-y-3" filters={filters} setFilters={setFilters} clearFilters={clearFilters} />
-            </FiltersSidePanel>
+            <CollectionFiltersPanel
+              cards={filteredCards}
+              filters={filters}
+              setFilters={setFilters}
+              clearFilters={clearFilters}
+              share={share}
+              missions={!isPublic}
+            />
           </SheetContent>
         </Sheet>
       ) : (
         <div className="w-80">
-          <FiltersSidePanel cards={filteredCards} share={share} isPublic={isPublic}>
-            <FilterPanel className="flex flex-col gap-y-3" filters={filters} setFilters={setFilters} clearFilters={clearFilters} />
-          </FiltersSidePanel>
+          <CollectionFiltersPanel
+            cards={filteredCards}
+            filters={filters}
+            setFilters={setFilters}
+            clearFilters={clearFilters}
+            share={share}
+            missions={!isPublic}
+          />
         </div>
       )}
       <div className="w-full max-w-[900px]">
