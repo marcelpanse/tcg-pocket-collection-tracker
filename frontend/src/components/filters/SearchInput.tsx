b8f01af470f7e2ef4266e99b2c0d78bcb00e635c
Grzegorz Kwacz <gkwacz@hotmail.com>
2025-12-01 19:32:00 +0100

    Remove search debounce (#765)

--- frontend/src/components/filters/SearchInput.tsx
+++ frontend/src/components/filters/SearchInput.tsx
@@ -3,8 +3,6 @@ import { useTranslation } from 'react-i18next'
 import { Input } from '@/components/ui/input.tsx'
 import { cn } from '@/lib/utils'
 
-let _searchDebounce: number | null = null
-
 type Props = {
   value?: string
   setValue: (searchValue: string) => void
@@ -19,14 +17,7 @@ const SearchInput: FC<Props> = ({ value, setValue, className }) => {
       className={cn('border-2 h-[38px] bg-neutral-800', className)}
       style={{ borderColor: '#45556C' }}
       defaultValue={value}
-      onChange={(e) => {
-        if (_searchDebounce) {
-          window.clearTimeout(_searchDebounce)
-        }
-        _searchDebounce = window.setTimeout(() => {
-          setValue(e.target.value)
-        }, 500)
-      }}
+      onChange={(e) => setValue(e.target.value)}
     />
   )
 }
