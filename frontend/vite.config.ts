379a63ceb70924ba551d33dfb49b183bf8cce806
marcel panse <marcel.panse@gmail.com>
2025-12-20 19:42:08 +0100

    adding react-compiler

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,13 +1,22 @@
 import path from 'node:path'
 import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
 import stripComments from 'vite-plugin-strip-comments'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss(), stripComments({ type: 'none' }), mkcert()],
+  plugins: [
+    react({
+      babel: {
+        plugins: ['babel-plugin-react-compiler'],
+      },
+    }),
+    tailwindcss(),
+    stripComments({ type: 'none' }),
+    mkcert(),
+  ],
 
   resolve: {
     alias: {
