f27535c2a85172c5fa689a5f2e506c63bbe1bd1b
Grzegorz Kwacz <gkwacz@hotmail.com>
2025-12-24 13:01:01 +0100

    wip

---
+++ frontend/src/services/decks/deckService.ts
@@ -0,0 +1,38 @@
+import { supabase } from '@/lib/supabase'
+import type { Deck } from '@/types'
+
+export async function getDeck(id: number) {
+  const { data, error } = await supabase.from('decks').select('*').eq('id', id).maybeSingle()
+  if (error) {
+    console.error('supabase error', error)
+    throw new Error('Failed fetching decks')
+  }
+  return data as Deck | null
+}
+
+export async function getMyDecks() {
+  const { data, error } = await supabase.from('decks').select('*')
+  if (error) {
+    console.error('supabase error', error)
+    throw new Error('Failed fetching user decks')
+  }
+  return data as Deck[]
+}
+
+export async function updateDeck(deck: Deck) {
+  const { data, error } = await supabase.from('decks').upsert(deck).select().single()
+  if (error) {
+    console.error('supabase error', error)
+    throw new Error('Failed updating decks')
+  }
+  return data as Deck
+}
+
+export async function deleteDeck(id: number) {
+  const { data, error } = await supabase.from('decks').delete().eq('id', id).select().single()
+  if (error) {
+    console.error('supabase error', error)
+    throw new Error('Failed updating decks')
+  }
+  return data as Deck
+}
---
+++ frontend/src/services/decks/useDeck.ts
@@ -0,0 +1,50 @@
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query'
+import type { Deck } from '@/types'
+import { useUser } from '../auth/useAuth'
+import { deleteDeck, getDeck, getMyDecks, updateDeck } from './deckService'
+
+export function useDeck(id?: number) {
+  return useQuery({
+    queryKey: ['deck', id],
+    queryFn: () => getDeck(id as number),
+    enabled: id !== undefined,
+  })
+}
+
+export function useMyDecks() {
+  const { data: user } = useUser()
+  return useQuery({
+    queryKey: ['decks', 'my'],
+    queryFn: () => getMyDecks(),
+    enabled: !!user && !!user.user.email,
+  })
+}
+
+export function useUpdateDeck() {
+  const { data: user } = useUser()
+  const email = user?.user.email
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: (deck: Deck) => {
+      if (!email) {
+        throw new Error('Email is required to update deck')
+      }
+      return updateDeck({ ...deck, email })
+    },
+    onSuccess: (deck) => {
+      queryClient.setQueryData(['deck', deck.id], deck)
+      queryClient.invalidateQueries({ queryKey: ['decks', 'my'] }) // TODO: update it instead
+    },
+  })
+}
+
+export function useDeleteDeck() {
+  const queryClient = useQueryClient()
+  return useMutation({
+    mutationFn: deleteDeck,
+    onSuccess: (deck) => {
+      queryClient.invalidateQueries({ queryKey: ['deck', deck.id] })
+      queryClient.invalidateQueries({ queryKey: ['decks', 'my'] }) // TODO: update it instead
+    },
+  })
+}
