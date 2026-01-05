2d927058d08f353b1125556eeebe8599148e1f04
Grzegorz Kwacz <gkwacz@hotmail.com>
2026-01-05 15:27:02 +0100

    Improve ui

--- frontend/src/pages/decks/Decks.tsx
+++ frontend/src/pages/decks/Decks.tsx
@@ -1,4 +1,4 @@
-import { ChevronLeft, ChevronRight } from 'lucide-react'
+import { ChevronFirst, ChevronLeft, ChevronRight } from 'lucide-react'
 import { useState } from 'react'
 import { Link } from 'react-router'
 import ErrorAlert from '@/components/ErrorAlert'
@@ -16,47 +16,43 @@ export default function Decks() {
   const { data, isLoading, isError, error } = useDecksSearch(filters)
 
   return (
-    <div className="flex flex-col gap-2 mx-auto max-w-[900px] px-1">
-      <div className="flex gap-2 flex-col sm:flex-row-reverse sm:justify-between">
-        <Link to="/decks/edit" className="inline-block sm:max-w-48">
+    <div className="flex gap-4 flex-col sm:flex-row sm:w-fit mx-auto px-1">
+      <div className="flex flex-col gap-2">
+        <Link to="/decks/edit">
           <Button className="w-full">
             New deck
             <ChevronRight />
           </Button>
         </Link>
         <TabsFilter
-          className="w-full sm:w-fit"
+          className="w-full"
           options={deckKinds}
           value={filters.kind}
           onChange={(kind) => setFilters((prev) => ({ ...prev, kind }))}
           show={(kind) => `${kind.charAt(0).toUpperCase() + kind.slice(1)} decks`}
         />
+        <ToggleFilter options={energies} value={filters.energy} onChange={(energy) => setFilters((prev) => ({ ...prev, energy }))} show={showCardType} />
       </div>
-      <ToggleFilter
-        className="w-full sm:w-fit"
-        options={energies}
-        value={filters.energy}
-        onChange={(energy) => setFilters((prev) => ({ ...prev, energy }))}
-        show={showCardType}
-      />
-      <div className="flex items-center gap-2">
-        <span>Page {filters.page + 1}</span>
-        <Button variant="outline" onClick={() => setFilters((prev) => ({ ...prev, page: Math.max(prev.page - 1, 0) }))} disabled={filters.page <= 0}>
-          <ChevronLeft />
-        </Button>
-        <Button variant="outline" onClick={() => setFilters((prev) => ({ ...prev, page: prev.page + 1 }))} disabled={data?.length === 0}>
-          <ChevronRight />
-        </Button>
-      </div>
-      <div className="flex flex-col gap-2">
+      <div className="flex flex-col gap-2 sm:w-xl">
+        <div className="flex items-center gap-2">
+          <span>Page {filters.page + 1}</span>
+          <Button variant="outline" onClick={() => setFilters((prev) => ({ ...prev, page: 0 }))} disabled={filters.page <= 0}>
+            <ChevronFirst />
+          </Button>
+          <Button variant="outline" onClick={() => setFilters((prev) => ({ ...prev, page: Math.max(prev.page - 1, 0) }))} disabled={filters.page <= 0}>
+            <ChevronLeft />
+          </Button>
+          <Button variant="outline" onClick={() => setFilters((prev) => ({ ...prev, page: prev.page + 1 }))} disabled={!data?.hasNext}>
+            <ChevronRight />
+          </Button>
+          {data && <p className="italic text-neutral-400">Found {data.count} decks</p>}
+        </div>
         {isLoading ? (
           <Spinner size="md" overlay />
         ) : isError || !data ? (
           <ErrorAlert error={error ?? undefined} />
-        ) : data.length === 0 ? (
-          <p className="italic text-neutral-400">Nothing to show</p>
         ) : (
-          data.map((deck) => <DeckItem key={deck.id} deck={deck} />)
+          data.decks.map((deck) => <DeckItem key={deck.id} deck={deck} />)
         )}
       </div>
     </div>
--- frontend/src/services/decks/deckService.ts
+++ frontend/src/services/decks/deckService.ts
@@ -8,38 +8,43 @@ export interface DeckFilters {
   page: number
   energy: Energy[]
 }
-const pageSize = 25
 
 export async function getDeck(id: number) {
+  /* This function might look weird, but there are two reasons why it's like this:
+  1. We want to support the path `/decks/:id` for both private and public decks, and we can't deduce thich one it is beforehand.
+  2. Public decks have column `likes`, which private decks don't, and private decks have column `email` which public don't.
+     So to have both columns for a user looking at his public deck, we need to merge the results.
+  */
   const [personal, community] = await Promise.all([
     supabase.from('decks').select('*').eq('id', id).maybeSingle(),
     supabase.from('public_decks').select('*').eq('id', id).maybeSingle(),
   ])
-  let ret = {}
+  let res = {}
   if (!community.error && !!community.data) {
-    ret = { ...ret, ...community.data, is_public: true }
+    res = { ...res, ...community.data, is_public: true }
   }
   if (!personal.error && !!personal.data) {
-    ret = { ...ret, ...personal.data }
+    res = { ...res, ...personal.data }
   }
-  if (Object.keys(ret).length > 0) {
-    // some query succeded
-    return ret as Deck
+  if (Object.keys(res).length > 0) {
+    // at least one query succeded
+    return res as Deck
   }
   console.error('supabase error?', personal.error, community.error)
   throw new Error('Failed fetching deck')
 }
 
+const pageSize = 25
 export async function getDecks(filters: DeckFilters) {
   // biome-ignore lint: supabase query builder is hard to type
   let tbl: any = supabase
 
   if (filters.kind === 'my') {
-    tbl = tbl.from('decks').select('*')
+    tbl = tbl.from('decks').select('*', { count: 'exact' })
   } else if (filters.kind === 'liked') {
-    tbl = tbl.from('deck_likes').select('*, public_decks!id(*)')
+    tbl = tbl.from('deck_likes').select('*, public_decks!id(*)', { count: 'exact' })
   } else if (filters.kind === 'community') {
-    tbl = tbl.from('public_decks').select('*').order('likes', { ascending: false })
+    tbl = tbl.from('public_decks').select('*', { count: 'exact' }).order('likes', { ascending: false })
   }
 
   const col_prefix = filters.kind === 'liked' ? 'public_decks.' : ''
@@ -48,17 +53,19 @@ export async function getDecks(filters: DeckFilters) {
     tbl = tbl.contains(`${col_prefix}energy`, filters.energy)
   }
 
-  const { data, error } = await tbl.range(filters.page * pageSize, (filters.page + 1) * pageSize - 1)
+  const { data, count, error } = await tbl.range(filters.page * pageSize, (filters.page + 1) * pageSize - 1)
   if (error) {
     throw new Error(`Failed fetching decks: ${error.message}`)
   }
 
+  let decks: Deck[]
   if (filters.kind === 'liked') {
     // biome-ignore lint: joins are hard to type
-    return data.map((row: any) => ({ ...row.public_decks, is_public: true })) as Deck[]
+    decks = data.map((row: any) => ({ ...row.public_decks, is_public: true })) as Deck[]
   } else {
-    return data as Deck[]
+    decks = data as Deck[]
   }
+  return { decks, count, hasNext: (filters.page + 1) * pageSize < count }
 }
 
 export async function updateDeck(deck: Deck) {
