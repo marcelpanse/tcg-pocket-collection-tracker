2ad1d0ea72a916924c7fc9d262e2fc739e74529e
Grzegorz Kwacz <gkwacz@hotmail.com>
2026-04-23 11:01:02 +0200

    Amount wanted in SQL

--- scripts/sql/trade-partners.sql
+++ scripts/sql/trade-partners.sql
@@ -1,4 +1,4 @@
-EXPLAIN ANALYZE
+-- All matches
 WITH recent_accounts AS (
     SELECT email, friend_id, username, language
     FROM accounts
@@ -33,13 +33,13 @@ FROM
                         INNER JOIN trade_rarity_settings t ON t.email = $1 AND (ca.internal_id & 63) = t.rarity_id
                 WHERE
                     ca.email = $1
-                  AND ca.amount_owned > t.to_keep
+                  AND ca.amount_owned > COALESCE(ca.amount_wanted, t.to_keep)
             ) as to_give
                 CROSS JOIN recent_accounts a
                 LEFT JOIN card_amounts ca ON ca.email = a.email AND ca.internal_id = to_give.internal_id
                 INNER JOIN trade_rarity_settings t ON t.email = a.email AND (to_give.internal_id & 63) = t.rarity_id
         WHERE
-            COALESCE(ca.amount_owned, 0) < t.to_collect
+            COALESCE(ca.amount_owned, 0) < COALESCE(ca.amount_wanted, t.to_collect)
         GROUP BY a.friend_id, a.username, a.language, t.rarity_id
     ) give
         JOIN
@@ -59,13 +59,13 @@ FROM
                         LEFT JOIN card_amounts ca ON ca.email = $1 AND cl.internal_id = ca.internal_id
                         INNER JOIN trade_rarity_settings t ON t.email = $1 AND (cl.internal_id & 63) = t.rarity_id
                 WHERE
-                    t.to_collect > 0 AND (ca.amount_owned IS NULL OR ca.amount_owned < t.to_collect)
+                    COALESCE(ca.amount_owned, 0) < COALESCE(ca.amount_wanted, t.to_collect)
             ) as to_get
                 CROSS JOIN recent_accounts a
                 INNER JOIN card_amounts ca ON ca.email = a.email AND ca.internal_id = to_get.internal_id
                 INNER JOIN trade_rarity_settings t ON t.email = a.email AND (ca.internal_id & 63) = t.rarity_id
         WHERE
-            ca.amount_owned > t.to_keep
+            ca.amount_owned > COALESCE(ca.amount_wanted, t.to_keep)
         GROUP BY a.friend_id, a.username, a.language, t.rarity_id
     ) get
         ON give.friend_id = get.friend_id
@@ -74,3 +74,53 @@ FROM
 GROUP BY give.friend_id, give.username, give.language
 ORDER BY trade_matches DESC;
 
+-- Single card
+WITH recent_accounts AS (
+    SELECT a.email, a.friend_id, a.username, a.language, t.to_collect
+    FROM
+        accounts a
+        INNER JOIN trade_rarity_settings t ON t.email = a.email AND t.rarity_id = ($2::int & 63)
+    WHERE
+        a.email != $1
+        AND a.is_active_trading = TRUE
+        AND a.is_public = TRUE
+        AND a.collection_last_updated IS NOT NULL
+        AND EXISTS (
+            SELECT internal_id
+            FROM card_amounts ca
+            WHERE
+                ca.email = a.email
+                AND ca.internal_id = $2::int
+                AND ca.amount_owned > COALESCE(ca.amount_wanted, t.to_keep)
+        )
+        AND ($3::text IS NULL OR language = $3::text)
+    ORDER BY collection_last_updated DESC
+    LIMIT 50
+)
+SELECT
+    a.friend_id,
+    a.username,
+    a.language,
+    COUNT(*) as trade_matches
+FROM
+    (
+        SELECT internal_id
+        FROM card_amounts
+        WHERE
+            email = $1
+            AND (internal_id & 63) = ($2::int & 63)
+            AND amount_owned > COALESCE(
+                amount_wanted,
+                (
+                    SELECT to_keep
+                    FROM trade_rarity_settings
+                    WHERE email = $1 AND rarity_id = ($2::int & 63)
+                )
+            )
+    ) as to_give
+    CROSS JOIN recent_accounts a
+    LEFT JOIN card_amounts ca ON ca.email = a.email AND ca.internal_id = to_give.internal_id
+WHERE
+    COALESCE(ca.amount_owned, 0) < a.to_collect
+GROUP BY a.friend_id, a.username, a.language
+ORDER BY trade_matches DESC;
