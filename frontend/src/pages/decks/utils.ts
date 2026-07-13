654d465593980bccb8e1fc7c36ddfa71bdf50201
Grzegorz Kwacz <gkwacz@hotmail.com>
2026-07-13 14:35:39 +0200

    Redesign deck view (#980)
    * Redesign deck view
    
    * Add ordering
    
    * CSS

--- frontend/src/pages/decks/utils.ts
+++ frontend/src/pages/decks/utils.ts
@@ -1,5 +1,5 @@
 import { getCardByInternalId } from '@/lib/CardsDB'
-import type { Collection } from '@/types'
+import type { Card, Collection } from '@/types'
 
 export function getDeckCardCounts(cards: number[]) {
   const map = new Map<number, number>()
@@ -9,10 +9,51 @@ export function getDeckCardCounts(cards: number[]) {
   return [...map].toSorted(([a, _n1], [b, _n2]) => a - b)
 }
 
+function getDeckbuildingCopies(card: Card, ownedCards: Collection) {
+  return card.alternate_versions.reduce((acc, id2) => acc + (ownedCards.get(id2)?.amount_owned ?? 0), 0)
+}
+
 export function getMissingCardsCount(cards: [number, number][], ownedCards: Collection) {
-  return cards.reduce(
-    (acc, [id, amount]) =>
-      acc + Math.max(0, amount - (getCardByInternalId(id)?.alternate_versions?.reduce((acc2, id2) => acc2 + (ownedCards.get(id2)?.amount_owned ?? 0), 0) ?? 0)),
-    0,
-  )
+  return cards.reduce((acc, [id, amount]) => {
+    const card = getCardByInternalId(id)
+    if (!card) {
+      console.error(`Unknown internal_id: ${id}`)
+      return acc
+    }
+    return acc + Math.max(0, amount - getDeckbuildingCopies(card, ownedCards))
+  }, 0)
+}
+
+export function getOwnedStatus(cards: number[], ownedCards: Collection) {
+  const pastCopies = new Map<number, number>()
+  return cards
+    .map(getCardByInternalId)
+    .filter((c): c is Card => c !== undefined)
+    .toSorted(cmp)
+    .map((card) => {
+      const id = card.alternate_versions[0]
+      const curr = (pastCopies.get(id) ?? 0) + 1
+      pastCopies.set(id, curr)
+      return [id, curr <= getDeckbuildingCopies(card, ownedCards)] as [number, boolean]
+    })
+}
+
+function sortRank(card: Card) {
+  return card.card_type === 'pokémon'
+    ? 0
+    : card.evolution_type === 'item'
+      ? 1
+      : card.evolution_type === 'tool'
+        ? 2
+        : card.evolution_type === 'supporter'
+          ? 3
+          : card.evolution_type === 'stadium'
+            ? 4
+            : 5
+}
+
+function cmp(a: Card, b: Card) {
+  const ar = sortRank(a)
+  const br = sortRank(b)
+  return ar === br ? a.internal_id - b.internal_id : ar - br
 }
